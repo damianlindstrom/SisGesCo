b54a56bc5f1d3a637ed7721c2901a5f62a972b09
Damian Lindstrom <damianlindstrom@gmail.com>
2026-09-16 19:38:41 -0300

    Agregado Vercel.json para manejar las rutas en Vercel

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,4 @@
 export const environment = {
   production: true,
-  // Se completa con la URL real del backend cuando esté desplegado.
-  apiUrl: 'https://TU-BACKEND-DESPLEGADO/api',
+  apiUrl: 'https://sisgesco-be.onrender.com'
 };
